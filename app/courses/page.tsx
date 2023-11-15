886989b2aaf7ee5b5cbad50dc84de16037bfdd35
Teerapong <teera04164@gmail.com>
2023-11-15 22:03:14 +0700

    no message

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -67,7 +67,7 @@ const Page = (props: Props) => {
             <div className="w-full flex items-center flex-wrap">
               <div
                 className={`h-[35px] ${
-                  category === "All" ? "bg-[#ff9d03]" : "bg-[#5050cb]"
+                  category === "All" ? "bg-[#205ec3]" : "bg-[#4291cf]"
                 } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
                 onClick={() => setCategory("All")}
               >
@@ -79,8 +79,8 @@ const Page = (props: Props) => {
                     <div
                       className={`h-[35px] ${
                         category === item.title
-                          ? "bg-[#ff9d03]"
-                          : "bg-[#5050cb]"
+                          ? "bg-[#205ec3]"
+                          : "bg-[#4291cf]"
                       } m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`}
                       onClick={() => setCategory(item.title)}
                     >
